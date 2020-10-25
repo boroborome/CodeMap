9d11055b56a52650ec5a870d4a4fd20556b6ad93
Yushan Gao <ysgao@thoughtworks.com>
2020-10-25 10:58:38 +0800

    fix: fix issue Object is showed in result

--- src/main/java/com/happy3w/codemap/SignatureAnalyzer.java
+++ src/main/java/com/happy3w/codemap/SignatureAnalyzer.java
@@ -38,6 +38,8 @@ public class SignatureAnalyzer {
                     || ch == '<'
                     || ch == '>'
                     || ch == ';'
+                    || ch == '+'
+                    || ch == ':'
             ) {
                 return index;
             }
@@ -53,6 +55,8 @@ public class SignatureAnalyzer {
                     || ch == '<'
                     || ch == '>'
                     || ch == ';'
+                    || ch == '+'
+                    || ch == ':'
             ) {
                 continue;
             }
