302b7f220fa110597665682e10e4f67d2e3b04fb
Yushan Gao <ysgao@thoughtworks.com>
2020-11-18 21:40:12 +0800

    chore: Add route for new workspace

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {NewWorkspaceComponent} from "./pages/new-workspace/new-workspace.component";
 
 const routes: Routes = [
   // { path: '', pathMatch: 'full', redirectTo: '/welcome' },
+  { path: 'workspace/new', pathMatch: 'full', component: NewWorkspaceComponent },
 ];
 
 @NgModule({
