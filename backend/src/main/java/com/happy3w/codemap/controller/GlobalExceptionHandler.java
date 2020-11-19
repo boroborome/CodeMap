fbd5c5ea921db54f639766fad0f48e3a87c72d8a
Yushan Gao <ysgao@thoughtworks.com>
2020-11-19 21:47:15 +0800

    chore: Show log when error

--- backend/src/main/java/com/happy3w/codemap/controller/GlobalExceptionHandler.java
+++ backend/src/main/java/com/happy3w/codemap/controller/GlobalExceptionHandler.java
@@ -1,15 +1,18 @@
 package com.happy3w.codemap.controller;
 
 import com.happy3w.toolkits.message.MessageResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(value = Exception.class)
     public ResponseEntity<MessageResponse> exceptionHandler(Exception e) {
+        log.error(e.getMessage(), e);
         return new ResponseEntity(
                 MessageResponse.fromError(e.getMessage()),
                 HttpStatus.OK);
